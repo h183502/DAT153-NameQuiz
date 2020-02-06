facbcce94dcd54b792325bdba6147e3cae9f0ccb
h183502 <heine.fjeld@gmail.com>
2020-02-06 11:37:45 +0100

    Begynt på oblig2, lagt til prefName

--- app/src/main/java/com/example/namequiz/MainActivity.kt
+++ app/src/main/java/com/example/namequiz/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.namequiz
 
+import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.LinearLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +16,67 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
+        savePrefBtn.setOnClickListener{
+
+            val name = prefName.text.toString().trim()
+            val msg = "The preference name is now ${name}"
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
+            val editor = sharedPreferences.edit()
+
+            editor.putString("NAME", name)
+            editor.apply()
+            prefName.setText("")
+        }
+
+        showPrefBtn.setOnClickListener{
+
+            val name = sharedPreferences.getString("NAME", "")
+
+            infoPref.text = "Preference name: $name"
+            goBack.visibility = View.VISIBLE
+            showPrefBtn.visibility = View.INVISIBLE
+            savePrefBtn.visibility = View.INVISIBLE
+            prefName.visibility = View.INVISIBLE
+            infoPref.visibility = View.VISIBLE
+
+
+        }
+
+        revealBtn.setOnClickListener{
+            revealBtn.visibility = View.INVISIBLE
+            prefName.visibility = View.VISIBLE
+            savePrefBtn.visibility = View.VISIBLE
+            showPrefBtn.visibility = View.VISIBLE
+            infoPref.visibility = View.INVISIBLE
+        }
+
+        goBack.setOnClickListener{
+            revealBtn.visibility = View.VISIBLE
+            prefName.visibility = View.INVISIBLE
+            savePrefBtn.visibility = View.INVISIBLE
+            showPrefBtn.visibility = View.INVISIBLE
+            infoPref.visibility = View.INVISIBLE
+            goBack.visibility = View.INVISIBLE
+
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.settings){
+            Intent(this, SettingsActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     public fun onClickDatabase(view: View){
